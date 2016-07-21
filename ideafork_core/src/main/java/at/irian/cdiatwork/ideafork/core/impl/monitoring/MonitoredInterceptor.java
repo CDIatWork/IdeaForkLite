90d2439efe53750d720a9a9b59cb061fd566731c
Gerhard Petracek <gerhard.petracek@irian.at>
2016-07-21 13:03:49 +0200

    removed interceptor-strategy

--- ideafork_core/src/main/java/at/irian/cdiatwork/ideafork/core/impl/monitoring/MonitoredInterceptor.java
+++ ideafork_core/src/main/java/at/irian/cdiatwork/ideafork/core/impl/monitoring/MonitoredInterceptor.java
@@ -1,7 +1,10 @@
 package at.irian.cdiatwork.ideafork.core.impl.monitoring;
 
 import at.irian.cdiatwork.ideafork.core.api.monitoring.Monitored;
+import org.apache.deltaspike.core.util.AnnotationUtils;
+import org.apache.deltaspike.core.util.ProxyUtils;
 
+import javax.enterprise.inject.spi.BeanManager;
 import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
@@ -14,10 +17,55 @@ public class MonitoredInterceptor implements Serializable {
     private static final long serialVersionUID = 3503111146284126952L;
 
     @Inject
-    private MonitoredInterceptorStrategy interceptorStrategy;
+    private MonitoredStorage monitoredStorage;
+
+    @Inject
+    private MonitoringConfig monitoringConfig;
+
+    @Inject
+    private BeanManager beanManager;
 
     @AroundInvoke
     public Object intercept(InvocationContext ic) throws Exception {
-        return this.interceptorStrategy.intercept(ic);
+        long start = System.currentTimeMillis();
+
+        try {
+            return ic.proceed();
+        } finally {
+            Monitored monitored = extractMonitoredAnnotation(ic);
+            int maxThreshold = monitored.maxThreshold();
+
+            if (maxThreshold < 1) {
+                maxThreshold = this.monitoringConfig.methodInvocationThreshold();
+            }
+
+            if (isSlowInvocation(start, maxThreshold)) {
+                this.monitoredStorage.recordSlowMethod(ic.getTarget().getClass().getName() + "#" + ic.getMethod().getName());
+            }
+        }
+    }
+
+    private Monitored extractMonitoredAnnotation(InvocationContext ic) {
+        Monitored result = ic.getMethod().getAnnotation(Monitored.class);
+
+        if (result != null) {
+            return result;
+        }
+
+        Class<?> targetClass = ic.getTarget().getClass();
+
+        targetClass = ProxyUtils.getUnproxiedClass(targetClass);
+
+        result = targetClass.getAnnotation(Monitored.class);
+
+        if (result == null) {
+            return AnnotationUtils.findAnnotation(beanManager, targetClass.getAnnotations(), Monitored.class);
+        }
+
+        return result;
+    }
+
+    protected boolean isSlowInvocation(long start, int maxThreshold) {
+        return System.currentTimeMillis() - start > maxThreshold;
     }
 }
