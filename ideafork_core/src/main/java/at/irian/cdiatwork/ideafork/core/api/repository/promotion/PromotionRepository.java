e832dd36cf3beea900f0f4e962e22648fed5d34f
Gerhard Petracek <gerhard.petracek@irian.at>
2016-07-21 12:54:13 +0200

    unified repository handling

--- ideafork_core/src/main/java/at/irian/cdiatwork/ideafork/core/api/repository/promotion/PromotionRepository.java
+++ ideafork_core/src/main/java/at/irian/cdiatwork/ideafork/core/api/repository/promotion/PromotionRepository.java
@@ -3,15 +3,58 @@ package at.irian.cdiatwork.ideafork.core.api.repository.promotion;
 import at.irian.cdiatwork.ideafork.core.api.domain.idea.Idea;
 import at.irian.cdiatwork.ideafork.core.api.domain.promotion.PromotionRequest;
 import at.irian.cdiatwork.ideafork.core.api.domain.role.User;
-import at.irian.cdiatwork.ideafork.core.api.repository.GenericRepository;
+import at.irian.cdiatwork.ideafork.core.api.monitoring.Monitored;
+import at.irian.cdiatwork.ideafork.core.api.repository.change.EntityProcessor;
+import org.apache.deltaspike.data.api.EntityRepository;
+import org.apache.deltaspike.data.api.Repository;
+import org.apache.deltaspike.jpa.api.transaction.Transactional;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.util.Date;
 import java.util.List;
 
-public interface PromotionRepository extends GenericRepository<PromotionRequest> {
+@Monitored
+@EntityProcessor
 
-    List<PromotionRequest> loadRecentIdeaPromotions(User user, String searchHint);
+@Transactional
+@Repository
+public abstract class PromotionRepository implements EntityRepository<PromotionRequest, String> {
+    @Inject
+    private EntityManager entityManager;
 
-    void promoteIdea(PromotionRequest promotionRequest);
+    public List<PromotionRequest> loadRecentIdeaPromotions(User user, String searchHint) {
+        if ("*".equalsIgnoreCase(searchHint)) {
+            return entityManager.createQuery(
+                    "select pr from PromotionRequest pr where pr.promotedAt is null and pr.ideaForPromotion.author.nickName <> :currentNick order by pr.createdAt desc",
+                    PromotionRequest.class)
+                    .setParameter("currentNick", user.getNickName())
+                    .setFirstResult(0)
+                    .setMaxResults(10)
+                    .getResultList();
+        }
+        return entityManager.createQuery(
+                "select pr from PromotionRequest pr where pr.promotedAt is null and pr.ideaForPromotion.author.nickName <> :currentNick and (pr.ideaForPromotion.topic like :searchText or pr.ideaForPromotion.category like :searchText) order by pr.createdAt desc",
+                PromotionRequest.class)
+                .setParameter("currentNick", user.getNickName())
+                .setParameter("searchText", "%" + searchHint + "%")
+                .setFirstResult(0)
+                .setMaxResults(10)
+                .getResultList();
+    }
 
-    List<Idea> loadRecentlyPromotedIdeas(User user);
+    public void promoteIdea(PromotionRequest promotionRequest) {
+        PromotionRequest storedPromotionRequest = this.entityManager.merge(promotionRequest);
+        storedPromotionRequest.setPromotedAt(new Date());
+    }
+
+    public List<Idea> loadRecentlyPromotedIdeas(User user) {
+        return entityManager.createQuery(
+                "select pr.ideaForPromotion from PromotionRequest pr where pr.promotedAt is not null and pr.ideaForPromotion.author.nickName <> :currentNick order by pr.promotedAt desc",
+                Idea.class)
+                .setParameter("currentNick", user.getNickName())
+                .setFirstResult(0)
+                .setMaxResults(10)
+                .getResultList();
+    }
 }
--- ideafork_core/src/main/java/at/irian/cdiatwork/ideafork/core/api/repository/statistic/UserActionRepository.java
+++ ideafork_core/src/main/java/at/irian/cdiatwork/ideafork/core/api/repository/statistic/UserActionRepository.java
@@ -2,10 +2,35 @@ package at.irian.cdiatwork.ideafork.core.api.repository.statistic;
 
 import at.irian.cdiatwork.ideafork.core.api.domain.role.User;
 import at.irian.cdiatwork.ideafork.core.api.domain.statistic.UserAction;
-import at.irian.cdiatwork.ideafork.core.api.repository.GenericRepository;
+import at.irian.cdiatwork.ideafork.core.api.monitoring.Monitored;
+import at.irian.cdiatwork.ideafork.core.api.repository.change.EntityProcessor;
+import org.apache.deltaspike.data.api.EntityRepository;
+import org.apache.deltaspike.data.api.Repository;
+import org.apache.deltaspike.jpa.api.transaction.Transactional;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import java.util.Collections;
 import java.util.List;
 
-public interface UserActionRepository extends GenericRepository<UserAction> {
-    List<UserAction> loadLatestActivities(User user);
+@Monitored
+@EntityProcessor
+
+@Transactional
+@Repository
+public abstract class UserActionRepository implements EntityRepository<UserAction, String> {
+    @Inject
+    private EntityManager entityManager;
+
+    public List<UserAction> loadLatestActivities(User user) {
+        try {
+            return entityManager.createQuery("select ua from UserAction ua where ua.user = :user order by ua.createdAt desc", UserAction.class)
+                    .setParameter("user", user)
+                    .setMaxResults(10) //we be done via config like maxNumberOfHighestRatedCategories
+                    .getResultList();
+        } catch (NoResultException e) {
+            return Collections.emptyList();
+        }
+    }
 }
